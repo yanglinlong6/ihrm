9805799ecc38239db1d8920b5b8dc79ec6b48195
liangwei <banemail@gmail.com>
2022-09-27 17:06:33 +0800

    24-自定义校验函数说明

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -12,20 +12,48 @@
       <el-form-item label="密码" prop="password">
         <el-input v-model="formData.password" />
       </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input v-model="formData.checkPass" />
+      </el-form-item>
       <el-form-item>
-        <el-button type="primary" size="small">登录</el-button>
+        <el-button type="primary" size="small">注册</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
+    // 因为自定义校验函数, 只用一次, 无需methods声明
+    // 在这里创建一下临时使用即可
+    const checkPassFn = (rule, val, callback) => {
+      // 这个函数是触发校验时组件库自动执行, 默认会传入三个三叔
+      // rule 当前规则, val 当前用户输入的值, callback 回调函数
+      // 如果函数逻辑认为输入符合条件, 直接调用 callback()
+      // 如果逻辑认为填写有错, 调用 callback 时要带上 原生错误对象, new Error(字符)
+      if (val === this.formData.password) {
+        callback()
+      } else {
+        callback(new Error('两次输入密码必须相同'))
+      }
+    }
+    // 不能像下面那样写, 会影响this指向, 建议直接使用箭头函数即可
+    // function checkPassFn(rule, val, callback) {
+    //   console.log(this)
+    //   if (val === this.formData.password) {
+    //     callback()
+    //   } else {
+    //     callback(new Error('两次输入密码必须相同'))
+    //   }
+    // }
+
     return {
       formData: {
         mobile: '',
-        password: ''
+        password: '',
+        checkPass: ''
       },
       // 规则本身是个对象, 里面的属性跟数据同名
       rules: {
@@ -36,6 +64,11 @@ export default {
         password: [
           { required: true, message: '该项不能为空', trigger: 'blur' },
           { min: 6, max: 12, message: '密码在6-12位之间', trigger: 'blur' }
+        ],
+        checkPass: [
+          { required: true, message: '该项不能为空', trigger: 'blur' },
+          // 这里没办法定规则, 可以用自定义校验函数处理
+          { validator: checkPassFn, trigger: 'blur' }
         ]
       }
     }

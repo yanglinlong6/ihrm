131ff9717cb8f1d6a1df47426ee838381ae51b3e
liangwei <banemail@gmail.com>
2022-09-30 17:06:13 +0800

    自定义指令处理图片问题

--- src/main.js
+++ src/main.js
@@ -22,6 +22,21 @@ Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
+// 演示自定义指令操作
+// Vue.directive(名字, 配置对象(钩子函数))
+Vue.directive('fiximg', {
+  // 这里的钩子函数, 会在使用这个指令的元素生命周期中自动被执行
+  inserted(el) {
+    console.log('这里是元素插入到页面中的钩子')
+    console.log(el)
+    // 只要用这个指令, 就能在元素插入到页面中时进行控制
+    // 这里可以监听dom的错误事件, 然后替换 src
+    el.addEventListener('error', () => {
+      el.src = 'https://ts1.cn.mm.bing.net/th/id/R-C.cabcfca68022e4b249e29a92bd9654ab?rik=jwCLB%2fVm3i5miQ&riu=http%3a%2f%2fimg.ewebweb.com%2fuploads%2f20200225%2f14%2f1582610405-zMSjcLXrFZ.jpeg&ehk=OaCQAZ3ziI8jbU91qP%2bxga00QhzrQ6WHroEnltMa91E%3d&risl=&pid=ImgRaw&r=0'
+    })
+  }
+})
+
 new Vue({
   el: '#app',
   router,

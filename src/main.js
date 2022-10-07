07f9a1b3443fa51745c07e34049e6801b5441967
liangwei <banemail@gmail.com>
2022-10-07 08:54:24 +0800

    除了出错, 还可以处理数据为空的状态

--- src/main.js
+++ src/main.js
@@ -34,6 +34,8 @@ Vue.directive('fiximg', {
     el.addEventListener('error', () => {
       el.src = 'https://ts1.cn.mm.bing.net/th/id/R-C.cabcfca68022e4b249e29a92bd9654ab?rik=jwCLB%2fVm3i5miQ&riu=http%3a%2f%2fimg.ewebweb.com%2fuploads%2f20200225%2f14%2f1582610405-zMSjcLXrFZ.jpeg&ehk=OaCQAZ3ziI8jbU91qP%2bxga00QhzrQ6WHroEnltMa91E%3d&risl=&pid=ImgRaw&r=0'
     })
+    // 除了出错, 还可以处理数据为空的状态
+    el.src = el.src || 'https://ts1.cn.mm.bing.net/th/id/R-C.cabcfca68022e4b249e29a92bd9654ab?rik=jwCLB%2fVm3i5miQ&riu=http%3a%2f%2fimg.ewebweb.com%2fuploads%2f20200225%2f14%2f1582610405-zMSjcLXrFZ.jpeg&ehk=OaCQAZ3ziI8jbU91qP%2bxga00QhzrQ6WHroEnltMa91E%3d&risl=&pid=ImgRaw&r=0'
   }
 })
 

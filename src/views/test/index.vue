ab95b946305376f10ad793a8631775cb2549b56a
liangwei <banemail@gmail.com>
2022-10-09 15:42:19 +0800

    22-树形数据渲染的基本用法

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,44 +1,50 @@
 <template>
   <div>
-    <TestTable :data="list">
-      <!-- 还有具名插槽的用法 -->
-      <template v-slot:header>
-        <th>角色名</th>
-        <th>描述</th>
-      </template>
-      <!-- 这里是父组件, 如果直接写 item
-      会在当前父组件的 data 里面找(找不到)
-      这些数据的作用域只在子组件内部 -->
-      <!-- 通过作用域插槽实现, v-slot 接收一个对象, 包含内部所有的数据 -->
-      <template v-slot="{ row }">
-        <td>{{ row.name }}</td>
-        <td>{{ row.desc }}</td>
-      </template>
-    </TestTable>
+    <h1>测试树形结构</h1>
+    <!--
+      data 传入嵌套的数据数组
+      default-expand-all 默认展开所有
+      props 传入一个配置对象,可以设置默认label/children在后台数据中真实的字段
+     -->
+    <el-tree :data="list" :props="config" default-expand-all />
   </div>
 </template>
 
 <script>
-import TestTable from './test-table.vue'
 export default {
-  components: {
-    TestTable
-  },
+  // 1. 数据结构
   data() {
     return {
+      // 部门数组
+      // 默认情况下树形组件只认 label 作为名称, children 作为子数据
+      // 由于这些数据是后端api给出可能字段不一样
       list: [
-        { id: 1, name: '总经理', desc: '啥都能干' },
-        { id: 2, name: '实习生', desc: '啥都不能干' }
-      ]
+        { name: '人事部' },
+        { name: '行政部' },
+        {
+          // 如果一个部门有下属, 应该添加一个 children 字段
+          name: '技术部',
+          children: [
+            { name: '前端开发' },
+            {
+              name: '后端开发',
+              children: [
+                { name: 'Java' },
+                { name: 'PHP' }
+              ]
+            }
+          ]
+        },
+        { name: '市场部' }
+      ],
+      config: {
+        label: 'name'
+      }
     }
   }
 }
 </script>
 
-<style lang="scss">
-.testTable {
-  td, th {
-    border: 1px solid #888;
-  }
-}
+<style>
+
 </style>

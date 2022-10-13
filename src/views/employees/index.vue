5ee572243f046f3d6fbfc39cfcfae617ddb9d16f
liangwei <banemail@gmail.com>
2022-10-13 12:10:30 +0800

    09-插槽实现顶部工具条封装

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>员工页面</div>
+  <div class="page-wrapper">
+    <PageTools :show-before="true">
+      <!-- 前半部分内容 -->
+      <template #before>
+        一共666员工
+      </template>
+
+      <!-- 后半部分内容 -->
+      <template #after>
+        <el-button size="small">导出员工</el-button>
+        <el-button size="small">导入员工</el-button>
+        <el-button type="primary" size="small">添加员工</el-button>
+      </template>
+    </PageTools>
+  </div>
 </template>
 
 <script>
+import PageTools from '@/components/PageTools'
 export default {
-
+  components: {
+    PageTools
+  }
 }
 </script>
 

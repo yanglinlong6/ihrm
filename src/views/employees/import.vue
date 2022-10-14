4b8ff60b5108e8e9c84404701a5c741ecce8b3a3
liangwei <banemail@gmail.com>
2022-10-14 15:10:20 +0800

    14-花裤衩封装的上传excel组件获取数据的方法

--- src/views/employees/import.vue
+++ src/views/employees/import.vue
@@ -1,13 +1,22 @@
 
 <template>
   <div class="uploadExcelWrapper">
-    <UploadExcel />
+    <!-- 我想知道要传什么数据, 就看子组件的 props -->
+    <!-- 要向知道有什么事件可以监听 -->
+    <UploadExcel :on-success="onSuccess" />
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    onSuccess(data) {
+      console.log('表头', data.header)
+      console.log('内容', data.results)
+      // 这个组件的功能是传入一个 excel 文件
+      // 返回对象, 包括 header / results 分别是表头和每一行数据
+    }
+  }
 }
 </script>
 

04bb8ea67fa9c73cdf2eb0eff6804fee3aba3add
liangwei <banemail@gmail.com>
2022-10-14 16:04:25 +0800

    16-中文用户翻译成英文用户数据

--- src/views/employees/import.vue
+++ src/views/employees/import.vue
@@ -15,6 +15,46 @@ export default {
       console.log('内容', data.results)
       // 这个组件的功能是传入一个 excel 文件
       // 返回对象, 包括 header / results 分别是表头和每一行数据
+      // 得到的数据是
+      // [
+      //   {
+      //     姓名: '小王',
+      //     手机号: 1322312,
+      //     工号: 10001
+      //   }
+      // ]
+      // 后端要求的数据
+      // [
+      //   {
+      //     username: '小王',
+      //     mobile: 1322312,
+      //     workNumber: 10001
+      //   }
+      // ]
+      const newData = data.results.map(item => {
+        const newUser = {}
+        newUser.username = item['姓名']
+        newUser.timeOfEntry = item['入职日期']
+        newUser.workNumber = item['工号']
+        newUser.mobile = item['手机号']
+        newUser.correctionTime = item['转正日期']
+
+        // const dict = {
+        //   '入职日期': 'timeOfEntry',
+        //   '手机号': 'mobile',
+        //   '姓名': 'username',
+        //   '转正日期': 'correctionTime',
+        //   '工号': 'workNumber'
+        // }
+        // for (const key in item) {
+        //   console.log(key)
+        //   // 姓名 / 工号等等
+        //   newUser[dict[key]] = item[key]
+        // }
+        return newUser
+      })
+
+      console.log('新数据', newData)
     }
   }
 }

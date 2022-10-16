53f3981f747e404367004b00f9b85f6a640d9081
liangwei <banemail@gmail.com>
2022-10-16 17:07:56 +0800

    21-数据回显

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -1,10 +1,54 @@
 <template>
-  <div>详情页</div>
+  <div class="page-wrapper">
+    <el-card>
+      <el-tabs>
+        <el-tab-pane label="登录信息">
+          <el-form :model="formData" :rules="rules">
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="formData.username" />
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input v-model="formData.password" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" size="small">更新</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="个人详情">
+          个人详细隐私数据
+        </el-tab-pane>
+        <el-tab-pane label="岗位信息">
+          岗位工作相关信息
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import { getUserDetail } from '@/api/user'
 export default {
-  created() {
+  data() {
+    return {
+      userId: this.$route.params.id,
+      formData: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { min: 2, max: 5, message: '名字在2-5位之间', trigger: 'blur' },
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        password: [
+          { min: 6, max: 12, message: '密码在6-12位之间', trigger: 'blur' },
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  async created() {
     console.log('详情页')
     // $router 路由对象, 一般用来执行跳转函数之类
     // $route 路由信息, 可以读取当前页面的路由数据
@@ -12,6 +56,11 @@ export default {
     // 跳转时, 路由配置中 /:xxx 的路径参数, 可以通过 this.$route.params获取
     console.log('问号传参的查询字符串', this.$route.query)
     console.log('路径参数', this.$route.params)
+    const res = await getUserDetail(this.userId)
+    this.formData = {
+      ...res,
+      password: ''
+    }
   }
 }
 </script>

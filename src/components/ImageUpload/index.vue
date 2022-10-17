8328ba2c5707b32aa8426fd2f988dea7e9288ebb
liangwei <banemail@gmail.com>
2022-10-17 16:46:16 +0800

    清理上传组件默认图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -44,9 +44,7 @@ export default {
   data() {
     return {
       // 管理图片的数组, 其中每张图片都是一个对象数据, 重点是其中的 url 属性是图片地址
-      fileList: [
-        { url: 'https://ts1.cn.mm.bing.net/th/id/R-C.a6ac762414af42f34a1461853028434b?rik=5vQZkGCenyc9LA&riu=http%3a%2f%2fwww.gx8899.com%2fuploads%2fallimg%2f2017092012%2f3nhfrqrzygy-lp.jpg%3fx-oss-process%3dstyle%2f144x144&ehk=VRfryaW55ThPNuKfkUZQS5s8Wn8oiTQupAvN7oOehko%3d&risl=&pid=ImgRaw&r=0' }
-      ]
+      fileList: []
     }
   },
   methods: {

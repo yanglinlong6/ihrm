1b428355bd2d6142644841a3f583169d00817fd3
liangwei <banemail@gmail.com>
2022-10-17 11:05:10 +0800

    图片上传组件数据初步绑定

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -7,12 +7,17 @@
       样式相关
         list-type 组件样式
         class 动态类名控制上传图标
+      钩子
+        on-remove 删除图片时自动触发, 手动覆盖文件列表
      -->
     <el-upload
       action="#"
-      list-type="picture-card"
       :file-list="fileList"
+
+      list-type="picture-card"
       :class="{disable: fileList.length === 1}"
+
+      :on-remove="onRemove"
     >
       <i class="el-icon-plus" />
     </el-upload>
@@ -25,8 +30,15 @@ export default {
     return {
       // 管理图片的数组, 其中每张图片都是一个对象数据, 重点是其中的 url 属性是图片地址
       fileList: [
-        // { url: 'https://ts1.cn.mm.bing.net/th/id/R-C.a6ac762414af42f34a1461853028434b?rik=5vQZkGCenyc9LA&riu=http%3a%2f%2fwww.gx8899.com%2fuploads%2fallimg%2f2017092012%2f3nhfrqrzygy-lp.jpg%3fx-oss-process%3dstyle%2f144x144&ehk=VRfryaW55ThPNuKfkUZQS5s8Wn8oiTQupAvN7oOehko%3d&risl=&pid=ImgRaw&r=0' }
+        { url: 'https://ts1.cn.mm.bing.net/th/id/R-C.a6ac762414af42f34a1461853028434b?rik=5vQZkGCenyc9LA&riu=http%3a%2f%2fwww.gx8899.com%2fuploads%2fallimg%2f2017092012%2f3nhfrqrzygy-lp.jpg%3fx-oss-process%3dstyle%2f144x144&ehk=VRfryaW55ThPNuKfkUZQS5s8Wn8oiTQupAvN7oOehko%3d&risl=&pid=ImgRaw&r=0' }
       ]
+    }
+  },
+  methods: {
+    onRemove(file, fileList) {
+      // file 当前正在删除的图片
+      // fileList 是删除后最新的图片数组
+      this.fileList = fileList
     }
   }
 }

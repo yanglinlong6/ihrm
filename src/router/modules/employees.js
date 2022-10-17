ca21ac0d3be5f77165523f1e75ee7219cada24fb
liangwei <banemail@gmail.com>
2022-10-17 17:57:09 +0800

    24-创建打印页并跳转

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -25,6 +25,11 @@ export default {
       path: 'detail/:id',
       component: () => import('@/views/employees/detail'),
       hidden: true
+    },
+    {
+      path: 'print/:id',
+      component: () => import('@/views/employees/print'),
+      hidden: true
     }
   ]
 }
---
+++ src/views/employees/print.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>打印页</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

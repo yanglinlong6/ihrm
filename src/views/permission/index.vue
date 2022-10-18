1ab2e9c6fbc30b33de4b427884dbe14f829da841
liangwei <banemail@gmail.com>
2022-10-18 10:22:35 +0800

    编辑弹窗回显数据

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -13,22 +13,68 @@
         <el-table-column>
           <template v-slot="{row}">
             <el-button v-if="row.type === 1" type="text">添加子权限</el-button>
-            <el-button type="text">编辑权限</el-button>
+            <el-button type="text" @click="showEditDialog(row.id)">编辑权限</el-button>
             <el-button type="text">删除权限</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
+
+    <el-dialog :title="formData.id?`编辑权限`:'新增权限'" :visible="isShowDialog" @close="btnCancel">
+      <!-- 表单 -->
+      <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
+        <el-form-item label="权限名称" prop="name">
+          <el-input v-model="formData.name" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="权限标识" prop="code">
+          <el-input v-model="formData.code" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="权限描述">
+          <el-input v-model="formData.description" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="开启">
+          <el-switch
+            v-model="formData.enVisible"
+            active-value="1"
+            inactive-value="0"
+          />
+        </el-form-item>
+      </el-form>
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getPermissionList } from '@/api/permission'
+import { getPermissionList, getPermissionDetail } from '@/api/permission'
 import { listToTree } from '@/utils'
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      isShowDialog: false,
+      formData: {
+        name: '',
+        code: '',
+        description: '',
+        enVisible: '1'
+      },
+      rules: {
+        name: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        description: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ]
+      }
     }
   },
   created() {
@@ -40,7 +86,13 @@ export default {
       // 由于权限也有树形结构, 需要之前封装的转换函数
       this.list = listToTree(res, '0')
       console.log(this.list)
-    }
+    },
+    async showEditDialog(id) {
+      this.formData = await getPermissionDetail(id)
+      this.isShowDialog = true
+    },
+    btnOK() {},
+    btnCancel() {}
   }
 }
 </script>

ed9495149078fded26066a8003955c7c6bedde8a
liangwei <banemail@gmail.com>
2022-10-18 09:07:46 +0800

    权限页面布局和接口封装, 初步获取数据

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,10 +1,41 @@
 <template>
-  <div>权限页面</div>
+  <div class="page-wrapper">
+    <PageTools>
+      <template #after>
+        <el-button size="small" type="primary">添加权限</el-button>
+      </template>
+    </PageTools>
+    <el-card>
+      <el-table :data="list" border>
+        <el-table-column label="权限名称" prop="name" />
+        <el-table-column label="权限标识" prop="code" />
+        <el-table-column label="权限描述" prop="description" />
+        <el-table-column>
+          <el-button type="text">添加子权限</el-button>
+          <el-button type="text">编辑权限</el-button>
+          <el-button type="text">删除权限</el-button>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
 export default {
-
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    async getList() {
+      this.list = await getPermissionList()
+    }
+  }
 }
 </script>
 

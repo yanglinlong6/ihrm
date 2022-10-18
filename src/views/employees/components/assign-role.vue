2afa300dde1439ddd896655ce9d1b3992edde68c
liangwei <banemail@gmail.com>
2022-10-18 12:23:03 +0800

    获取全部角色渲染列表

--- src/views/employees/components/assign-role.vue
+++ src/views/employees/components/assign-role.vue
@@ -1,16 +1,33 @@
 <template>
   <el-dialog title="分配角色" :visible="isShowDialog">
-    内容
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox v-for="item in list" :key="item.id" :label="item.id">{{ item.name }}</el-checkbox>
+    </el-checkbox-group>
+    {{ checkList }}
   </el-dialog>
 </template>
 
 <script>
+import { getRoleList } from '@/api/setting'
 export default {
   props: {
     isShowDialog: {
       type: Boolean,
       default: false
     }
+  },
+  data() {
+    return {
+      // 选中的列表
+      checkList: [],
+      // 所有角色总表
+      list: []
+    }
+  },
+  async created() {
+    const { rows } = await getRoleList({ page: 1, pagesize: 999 })
+    this.list = rows
+    console.log(this.list)
   }
 }
 </script>

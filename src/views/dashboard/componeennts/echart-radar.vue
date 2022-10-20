4fba9948d52f149fc39d07d5c44fc019199e7bb9
liangwei <banemail@gmail.com>
2022-10-20 15:48:58 +0800

    14-日历和雷达图组件创建

---
+++ src/views/dashboard/componeennts/echart-radar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>此处应有雷达图</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/dashboard/componeennts/work-calendar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    
+    <el-calendar v-model="currentDate" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    }
+  }
+}
+</script>
+
+<style  scoped>
+ /deep/ .el-calendar-day {
+  height:  auto;
+ }
+ /deep/ .el-calendar-table__row td,/deep/ .el-calendar-table tr td:first-child,  /deep/ .el-calendar-table__row td.prev{
+  border:none;
+ }
+.date-content {
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  font-size: 14px;
+}
+.date-content .rest {
+  color: #fff;
+  border-radius: 50%;
+  background: rgb(250, 124, 77);
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+  font-size: 12px;
+  margin-left: 10px;
+}
+.date-content .text{
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+ display: inline-block;
+
+}
+ /deep/ .el-calendar-table td.is-selected .text{
+   background: #409eff;
+   color: #fff;
+   border-radius: 50%;
+ }
+ /deep/ .el-calendar__header {
+   display: none
+ }
+</style>

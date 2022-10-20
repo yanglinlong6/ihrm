d72dad65caf3cf5b7195f242960bdda2f6876ad9
liangwei <banemail@gmail.com>
2022-10-20 08:59:45 +0800

    权限筛选测试

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -6,10 +6,13 @@
 import { constantRoutes } from '@/router'
 export default {
   created() {
+    // 权限标识的路由筛选作用
     const oldRoutes = constantRoutes
     const perm = this.$store.state.user.userInfo.roles.menus
-    console.log('所有路由', constantRoutes)
-    console.log('个人权限列表', this.$store.state.user.userInfo.roles.menus)
+
+    console.log('所有路由', oldRoutes)
+    console.log('个人权限列表', perm)
+
     const newRoutes = oldRoutes.filter(item => {
       return perm.indexOf(item.name) !== -1
     })

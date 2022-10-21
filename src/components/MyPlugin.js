726660fd2de0cb2a7872ff2c711641e12175db1a
liangwei <banemail@gmail.com>
2022-10-21 09:37:15 +0800

    03-全屏组件的使用说明

--- src/components/MyPlugin.js
+++ src/components/MyPlugin.js
@@ -1,6 +1,7 @@
 import PageTools from '@/components/PageTools'
 import UploadExcel from '@/components/UploadExcel'
 import ImageUpload from '@/components/ImageUpload'
+import ScreenFull from '@/components/ScreenFull'
 export default {
   install(Vue, data) {
     // 这个函数会在 Vue.use 时被调用
@@ -13,6 +14,7 @@ export default {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
     Vue.component('ImageUpload', ImageUpload)
+    Vue.component('ScreenFull', ScreenFull)
     // 组件库的作者以后, 只需要维护这个 install 方法即可
     // 有几个组件就多注册几个
     // 对于组件库的使用者, 无需其他变更
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -13,6 +13,8 @@
     </div>
 
     <div class="right-menu">
+      <ScreenFull class="right-menu-item" />
+      <!-- 员工头像下拉菜单 -->
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img v-fiximg :src="avatar" class="user-avatar">
@@ -123,7 +125,7 @@ export default {
         height: 100%;
         font-size: 18px;
         color: #5a5e66;
-        vertical-align: text-bottom;
+        vertical-align: middle;
 
         &.hover-effect {
           cursor: pointer;
